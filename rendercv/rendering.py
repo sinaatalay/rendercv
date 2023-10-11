e37630e890e051b6ead57bc07597664cfe92013a
Sina Atalay <sinatalay@hotmail.com>
2023-10-11 18:01:36 +0200

    debug run_latex tests

--- rendercv/rendering.py
+++ rendercv/rendering.py
@@ -391,7 +391,7 @@ def run_latex(latex_file_path):
             f"{latex_file}",
         ],
         cwd=os.path.dirname(latex_file_path),
-        # stdout=subprocess.DEVNULL,  # suppress latexmk output
+        stdout=subprocess.DEVNULL,  # suppress latexmk output
     )
     end_time = time.time()
     time_taken = end_time - start_time
