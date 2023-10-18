8285fe7c794df3b5167e738d7c241eee24612f5a
Sina Atalay <sinatalay@hotmail.com>
2023-10-18 19:33:19 +0200

    move entry function to __main__.py

--- rendercv/__main__.py
+++ rendercv/__main__.py
@@ -1,31 +1,20 @@
-"""
-This module is a script to run the RenderCV and generate a CV as a PDF. It is an entry
-point for the RenderCV package.
-"""
-import os
-import logging
 import sys
+import os
 
-from rendercv.rendering import read_input_file, render_template, run_latex
+from .rendering import read_input_file, render_template, run_latex
 
 
-def main(args=sys.argv[1:]):
-    """
-    This is the main function to run RenderCV.
-    """
-    logger = logging.getLogger(__name__)
-
-    if len(args) != 1:
+def main():
+    if len(sys.argv) < 2:
         raise ValueError("Please provide the input file path.")
-    elif len(args) == 1:
-        input_file_path = args[0]
+    elif len(sys.argv) == 2:
+        input_file_path = sys.argv[1]
     else:
         raise ValueError(
             "More than one input is provided. Please provide only one input, which is"
             " the input file path."
         )
 
-    # input_file_path = sys.argv[1]
     file_path = os.path.join(os.getcwd(), input_file_path)
     data = read_input_file(file_path)
     output_latex_file = render_template(data)
@@ -33,4 +22,4 @@ def main(args=sys.argv[1:]):
 
 
 if __name__ == "__main__":
-    main(args=["tests/inputs/personal.yaml"])
+    main()
