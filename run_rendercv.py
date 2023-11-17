66294343a20febba6f838efc971350271b998e8b
Sina Atalay <sinatalay@hotmail.com>
2023-11-17 16:27:10 +0100

    fix run_rendercv.py

--- run_rendercv.py
+++ run_rendercv.py
@@ -2,7 +2,7 @@ from rendercv.__main__ import render
 from rendercv.data_model import generate_json_schema
 import os
 
-input_file_path = "SinaAtalay_CV.yaml"
+input_file_path = "John_Doe_CV.yaml"
 render(input_file_path) # type: ignore
 
 # This script is equivalent to running the following command in the terminal:
