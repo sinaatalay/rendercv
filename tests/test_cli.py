4c1ecb711eefc181f4cbe173439bbd288efffc66
Sina Atalay <sinatalay@hotmail.com>
2023-11-18 16:25:20 +0100

    update tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,17 +12,16 @@ class TestCLI(unittest.TestCase):
         # Change the working directory to the root of the project:
         workspace_path = os.path.dirname(os.path.dirname(__file__))
 
-        with self.subTest(msg="Correct input"):
-            test_input_file_path = os.path.join(
-                workspace_path,
-                "tests",
-                "reference_files",
-                "John_Doe_CV_yaml_reference.yaml",
-            )
-            subprocess.run(
-                [sys.executable, "-m", "rendercv", "render", test_input_file_path],
-                check=True,
-            )
+        test_input_file_path = os.path.join(
+            workspace_path,
+            "tests",
+            "reference_files",
+            "John_Doe_CV_yaml_reference.yaml",
+        )
+        subprocess.run(
+            [sys.executable, "-m", "rendercv", "render", test_input_file_path],
+            check=True,
+        )
 
             # Read the necessary information and remove the output directory:
             output_file_path = os.path.join(workspace_path, "output", "John_Doe_CV.pdf")
@@ -33,18 +32,15 @@ class TestCLI(unittest.TestCase):
             # Check if the output file exists:
             self.assertTrue(file_exists, msg="PDF file couldn't be generated.")
 
-            # Compare the pdf file with the reference pdf file:
-            reference_pdf_file = os.path.join(
-                workspace_path,
-                "tests",
-                "reference_files",
-                "John_Doe_CV_pdf_reference.pdf",
-            )
-            reference_pdf_file_size = os.path.getsize(reference_pdf_file)
-            ratio = min(reference_pdf_file_size, pdf_file_size) / max(
-                reference_pdf_file_size, pdf_file_size
-            )
-            self.assertTrue(ratio > 0.98, msg="PDF file didn't match the reference.")
+        # Compare the pdf file with the reference pdf file:
+        reference_pdf_file = os.path.join(
+            workspace_path, "tests", "reference_files", "John_Doe_CV_pdf_reference.pdf"
+        )
+        reference_pdf_file_size = os.path.getsize(reference_pdf_file)
+        ratio = min(reference_pdf_file_size, pdf_file_size) / max(
+            reference_pdf_file_size, pdf_file_size
+        )
+        self.assertTrue(ratio > 0.98, msg="PDF file didn't match the reference.")
 
         # Wrong input:
         with self.subTest(msg="Wrong input"):
