7d4260f8e7d43a7efaea63b016645439f595e8ef
Sina Atalay <sinatalay@hotmail.com>
2024-02-04 21:48:50 +0100

    remove old __init__.py code

--- rendercv/__init__.py
+++ rendercv/__init__.py
@@ -2,42 +2,7 @@
 
 To be continued...
 """
+
 __version__ = "1.0"
 
-import logging
-import os
-import sys
-
-
-class LoggingFormatter(logging.Formatter):
-    grey = "\x1b[38;20m"  # debug level
-    white = "\x1b[37;20m"  # info level
-    yellow = "\x1b[33;20m"  # warning level
-    red = "\x1b[31;20m"  # error level
-    bold_red = "\x1b[31;1m"  # critical level
-
-    reset = "\x1b[0m"
-    format = "%(levelname)s | %(message)s"  # type: ignore
-
-    FORMATS = {
-        logging.DEBUG: grey + format + reset,  # type: ignore
-        logging.INFO: white + format + reset,  # type: ignore
-        logging.WARNING: yellow + format + reset,  # type: ignore
-        logging.ERROR: red + format + reset,  # type: ignore
-        logging.CRITICAL: bold_red + format + reset,  # type: ignore
-    }
-
-    def format(self, record):
-        log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt)
-        return formatter.format(record)
-
-
-# Initialize logger with colors
-if sys.platform == "win32":
-    os.system("COLOR 0")  # enable colors in Windows terminal
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-stdout_handler = logging.StreamHandler()
-stdout_handler.setFormatter(LoggingFormatter())
-logger.addHandler(stdout_handler)
+from rendercv.__main__ import render, new
