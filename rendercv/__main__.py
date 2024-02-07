48d48f4bf2638d27e7378cba59a3b3dc599d242c
Sina Atalay <sinatalay@hotmail.com>
2024-02-07 19:23:40 +0100

    update __main__.py

--- rendercv/__main__.py
+++ rendercv/__main__.py
@@ -1,3 +1,9 @@
+"""
+`__main__.py` file is the file that gets executed when the RenderCV package itself is
+invoked directly from the command line with `python -m rendercv`. That's why we have it
+here so that we can invoke the CLI from the command line with `python -m rendercv`.
+"""
+
 from .cli import cli
 
 if __name__ == "__main__":
