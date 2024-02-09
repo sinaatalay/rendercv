59a9a678898920537aaa1aad20ce4c50fd23aefc
Sina Atalay <sinatalay@hotmail.com>
2024-02-09 20:15:08 +0100

    update __init__.py

--- rendercv/__init__.py
+++ rendercv/__init__.py
@@ -3,7 +3,8 @@
 To be continued...
 """
 
-__version__ = "1.0"
-
 from rendercv.cli import render, new
 from rendercv.data_models import generate_json_schema_file
+
+__version__ = "1.0"
+__all__ = ["render", "new", "generate_json_schema_file", "__version__"]
