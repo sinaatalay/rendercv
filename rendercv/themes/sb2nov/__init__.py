945a33a176cb149cdff09d35ce46b395a1be9c73
Sina Atalay <sinatalay@hotmail.com>
2024-02-15 19:30:21 +0100

    finalize sb2nov theme

--- rendercv/themes/sb2nov/__init__.py
+++ rendercv/themes/sb2nov/__init__.py
@@ -17,70 +17,59 @@ class Sb2novThemeOptions(pydantic.BaseModel):
     model_config = pydantic.ConfigDict(extra="forbid")
 
     theme: Literal["sb2nov"]
+
     font_size: Literal["10pt", "11pt", "12pt"] = pydantic.Field(
         default="10pt",
         title="Font Size",
-        description='The font size of the CV. The default value is "10pt".',
-        examples=["10pt", "11pt", "12pt"],
+        description="The font size of the CV. It can be 10pt, 11pt, or 12pt.",
     )
     page_size: Literal["a4paper", "letterpaper"] = pydantic.Field(
         default="a4paper",
         title="Page Size",
-        description='The page size of the CV. The default value is "a4paper".',
-        examples=["a4paper", "letterpaper"],
+        description="The page size of the CV. It can be a4paper or letterpaper.",
     )
-    color: (
-        Literal["blue"]
-        | Literal["black"]
-        | Literal["burgundy"]
-        | Literal["green"]
-        | Literal["grey"]
-        | Literal["orange"]
-        | Literal["purple"]
-        | Literal["red"]
-    ) = pydantic.Field(
-        default="blue",
-        validate_default=True,
-        title="Primary Color",
-        description='The primary color of the CV. The default value is "blue".',
-        examples=[
-            "blue",
-            "black",
-            "burgundy",
-            "green",
-            "grey",
-            "orange",
-            "purple",
-            "red",
-        ],
+    disable_page_numbering: bool = pydantic.Field(
+        default=False,
+        title="Disable Page Numbering",
+        description=(
+            "If this option is set to true, then the page numbering will be disabled."
+        ),
     )
-    date_width: LaTeXDimension = pydantic.Field(
-        default="3.8 cm",
-        validate_default=True,
+    date_and_location_width: LaTeXDimension = pydantic.Field(
+        default="4.1 cm",
         title="Date and Location Column Width",
-        description='The width of the date column. The default value is "3.8 cm".',
+        description="The width of the date and location column.",
     )
-    content_scale: float = pydantic.Field(
-        default=0.75,
-        title="Content Scale",
+    space_between_connection_objects: LaTeXDimension = pydantic.Field(
+        default="0.5 cm",
+        title="Space Between Connection Objects",
         description=(
-            "The scale of the content with respect to the page size. The default value"
-            ' is "0.75".'
+            "The space between the connection objects (like phone, email, and website)."
         ),
     )
-    show_only_years: bool = pydantic.Field(
-        default=False,
-        title="Show Only Years",
+    text_alignment: Literal["left-aligned", "justified"] = pydantic.Field(
+        default="left-aligned",
+        title="Text Alignment",
+        description="The alignment of the text.",
+    )
+    show_timespan_in: list[str] = pydantic.Field(
+        default=[],
+        title="Show Time Span in These Sections",
         description=(
-            'If "True", only the years will be shown in the date column. The default'
-            ' value is "False".'
+            "The time span will be shown in the date and location column in these"
+            " sections. The input should be a list of strings."
         ),
     )
-    disable_page_numbers: bool = pydantic.Field(
-        default=False,
-        title="Disable Page Numbers",
+    show_last_updated_date: bool = pydantic.Field(
+        default=True,
+        title="Show Last Updated Date",
         description=(
-            'If "True", the page numbers will be disabled. The default value is'
-            ' "False".'
+            "If this option is set to true, then the last updated date will be shown"
+            " in the header."
         ),
+    )
+    header_font_size: LaTeXDimension = pydantic.Field(
+        default="30 pt",
+        title="Header Font Size",
+        description="The font size of the header (the name of the person).",
     )
