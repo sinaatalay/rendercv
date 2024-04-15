034ef7f0560d684a815ac33c0f14707bbc1d17e5
Sina Atalay <sinatalay@hotmail.com>
2024-04-16 02:38:09 +0300

    sb2nov: switch to RenderCV's LaTeX code

--- rendercv/themes/sb2nov/__init__.py
+++ rendercv/themes/sb2nov/__init__.py
@@ -2,14 +2,35 @@ from typing import Literal
 
 import pydantic
 
-from .. import ThemeOptions, LaTeXDimension
+from .. import ThemeOptions, LaTeXDimension, Margins, EntryAreaMargins
+
+
+class EntryAreaMarginsForSb2nov(EntryAreaMargins):
+    """This class is a data model for the entry area margins."""
+
+    bullet_width: LaTeXDimension = pydantic.Field(
+        default="0.6 cm",
+        title="Width of the Entry Bullet",
+        description=(
+            "The width of the bullet for each entry. The default value is 1 cm."
+        ),
+    )
+
+
+class MarginsForSb2nov(Margins):
+    """This class is a data model for the margins."""
+
+    entry_area: EntryAreaMarginsForSb2nov = pydantic.Field(
+        default=EntryAreaMarginsForSb2nov(),
+        title="Entry Area Margins",
+        description="Entry area margins.",
+    )
 
 
 class Sb2novThemeOptions(ThemeOptions):
-    """This class is the data model of the theme options for the sb2nov theme."""
+    """This class is the data model of the theme options for the `sb2nov` theme."""
 
     theme: Literal["sb2nov"]
-
     header_font_size: LaTeXDimension = pydantic.Field(
         default="24 pt",
         title="Header Font Size",
@@ -18,4 +39,9 @@ class Sb2novThemeOptions(ThemeOptions):
             " 24 pt. Unfortunately, sb2nov does not support font sizes bigger than"
             " 24 pt."
         ),
+    )
+    margins: MarginsForSb2nov = pydantic.Field(
+        default=MarginsForSb2nov(),
+        title="Margins",
+        description="Page, section title, entry field, and highlights field margins.",
     )
