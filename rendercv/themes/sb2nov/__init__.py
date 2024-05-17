79c2ac1ba6fd69a178d6e2afd6693b50e95b8e53
Sina Atalay <sinatalay@hotmail.com>
2024-05-17 15:59:12 +0300

    sb2nov: fix margin problems

--- rendercv/themes/sb2nov/__init__.py
+++ rendercv/themes/sb2nov/__init__.py
@@ -5,28 +5,6 @@ import pydantic
 from .. import ThemeOptions, LaTeXDimension, Margins, EntryAreaMargins
 
 
-class EntryAreaMarginsForSb2nov(EntryAreaMargins):
-    """This class is a data model for the entry area margins."""
-
-    bullet_width: LaTeXDimension = pydantic.Field(
-        default="0.6 cm",
-        title="Width of the Entry Bullet",
-        description=(
-            "The width of the bullet for each entry. The default value is 0.6 cm."
-        ),
-    )
-
-
-class MarginsForSb2nov(Margins):
-    """This class is a data model for the margins."""
-
-    entry_area: EntryAreaMarginsForSb2nov = pydantic.Field(
-        default=EntryAreaMarginsForSb2nov(),
-        title="Entry Area Margins",
-        description="Entry area margins.",
-    )
-
-
 class Sb2novThemeOptions(ThemeOptions):
     """This class is the data model of the theme options for the `sb2nov` theme."""
 
@@ -52,9 +30,4 @@ class Sb2novThemeOptions(ThemeOptions):
             " 24 pt. Unfortunately, sb2nov does not support font sizes bigger than"
             " 24 pt."
         ),
-    )
-    margins: MarginsForSb2nov = pydantic.Field(
-        default=MarginsForSb2nov(),
-        title="Margins",
-        description="Page, section title, entry field, and highlights field margins.",
     )
