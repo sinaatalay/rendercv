ef733e1c33db8057aca305aebf5514f4d54825da
Sina Atalay <sinatalay@hotmail.com>
2024-05-29 16:05:51 +0300

    tests: test Google Scholar

--- tests/test_data_models.py
+++ tests/test_data_models.py
@@ -450,6 +450,11 @@ def test_invalid_social_networks(network, username):
             "@myusername",
             "https://youtube.com/@myusername",
         ),
+        (
+            "Google Scholar",
+            "myusername",
+            "https://scholar.google.com/citations?user=myusername",
+        ),
     ],
 )
 def test_social_network_url(network, username, expected_url):
