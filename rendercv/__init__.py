d8533f3e396f0c0568a1ab175ce4189dd42c5eea
Sina Atalay <sinatalay@hotmail.com>
2024-07-13 19:47:09 +0300

    docs: update docstring of the `rendercv/__init__.py`

--- rendercv/__init__.py
+++ rendercv/__init__.py
@@ -1,5 +1,4 @@
-"""RenderCV package.
-
+"""
 RenderCV is a $\\LaTeX$ CV/resume framework that allows you to create a high-quality CV
 as a PDF from a YAML file with Markdown syntax support and complete control over the
 $\\LaTeX$ code.
