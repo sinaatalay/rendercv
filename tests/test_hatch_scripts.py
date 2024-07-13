afd35c20a7497c6619f0bc742b4fc2632f08b653
Sina Atalay <sinatalay@hotmail.com>
2024-07-13 19:31:53 +0300

    tests: add hatch scripts tests

---
+++ tests/test_hatch_scripts.py
@@ -0,0 +1,24 @@
+import subprocess
+import sys
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "script_name",
+    [
+        "format",
+        "lint",
+        "sort-imports",
+        "check-types",
+        "docs:build",
+        "docs:update-schema",
+        "docs:update-examples",
+        "docs:update-entry-figures",
+        # "docs:serve",
+        # "test:run",
+        # "test:run-and-report",
+    ],
+)
+def test_default_format(script_name):
+    subprocess.run([sys.executable, "-m", "hatch", "run", script_name])
