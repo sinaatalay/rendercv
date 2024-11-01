c3433209d4354269eb93ed247d9fc27b5fec906f
Sina Atalay <dev@atalay.biz>
2024-11-01 19:52:55 -0400

    tests: revert update_testdata to False

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,7 +23,7 @@ from rendercv.renderer import templater
 # the output. Setting update_testdata to True will update the reference files with
 # the latest RenderCV. This should be done with caution, as it will overwrite the
 # reference files with the latest output.
-update_testdata = True
+update_testdata = False
 
 # copy sample entries from docs/update_rendercv_files.py:
 education_entry_dictionary = {
